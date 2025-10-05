161ba17c8c5574dcbee03aaf3ff23bd1926fdd7d
NRacette <Nicholasracette1@gmail.com>
2025-10-05 13:29:53 -0500

    first commit

---
+++ src/Driver.java
@@ -0,0 +1,123 @@
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * Description: This program takes unsorted arrays of integers and sorts them using two different quicksort methods.
+ * The breakdown of the program is, first it creates the 3 arrays and fills them with sorted, random, and reversed values.
+ * Then calls the quicksort method using the first as pivot and then median as pivot on all 3 arrays printing and timing
+ * for each of the arrays. Using the runTest method.
+ * The quicksorts break down the array into sub arrays by using a partition method and recursively calling itself on the sub arrays
+ * This recursion continues until the array is broken down to base cases where the sub arrays are of length 1 or 0.
+ *
+ * INPUT:
+ *      CONSTANTS:
+ *          ARRAY_SIZE - size of the array to be sorted
+ * 		Input:
+ * 			Sorted, random, and reversed arrays of integers length 10.
+ * 
+ *
+ * 	COMPUTATION:
+ * 		1. Create an array of unsorted integers.
+ * 		2. Call two different quicksort methods to sort the array.
+ * 		3. returns the sorted arrays
+ *      4. Calculate time taken for each sort method.
+ *
+ * 	OUTPUT:
+ * 		Sorted array using the two different quicksort methods.
+ *      Times for each sort method.
+ * 
+ * 	@author: Nick Racette
+ * 	@contact: Nick.Racette@go.minnestate.edu
+ * 	@since: 10/05/2024
+ * 
+ * 	Course: CSCI 2082-70 Data Structures and Algorithms
+ *
+ * 	Institution: Century College
+ * 
+ * 	Instructor: Mathew Nyamgawa
+ */
+public class Driver {
+
+    /**
+     * The main method that executes the testing for each quicksort method.
+     *
+     * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+
+
+        QuickSort sorter = new QuickSort();
+        final int ARRAY_SIZE = 10;
+
+        // Create test arrays size 10 and Random object
+        int[] sortedArrayOriginal = new int[ARRAY_SIZE];
+        int[] randomArrayOriginal = new int[ARRAY_SIZE];
+        int[] reversedArrayOriginal = new int[ARRAY_SIZE];
+        Random randomValue = new Random();
+
+        // Fill the arrays with sorted, random, and reversed values
+        for (int i = 0; i < ARRAY_SIZE; i++) {
+            sortedArrayOriginal[i] = (i + 1) * 5;
+            randomArrayOriginal[i] = randomValue.nextInt(100);
+            reversedArrayOriginal[i] = (ARRAY_SIZE - i) * 5;
+        }
+
+        
+        System.out.println("--------------------------------------------------------------------------");
+        System.out.println("      Quicksort Algorithmes      ");
+        System.out.println("      By: Nicholas Racette      ");
+        System.out.println("--------------------------- START OF SORTING ---------------------------");
+        /**
+         * Time complexity quickSortFirst  O(n^2) -- worst case
+         * Time complexity quickSortMedian  O(n*log(n)) -- average case
+         */
+        runTest("An array with sorted values", sortedArrayOriginal, sorter);
+        /**
+         * Time complexity quickSortFirst  O(n*log(n)) -- average case
+         * Time complexity quickSortMedian  O(n*log(n)) -- average case
+         */
+        runTest("An array with randomly ordered values", randomArrayOriginal, sorter);
+        /**
+         * Time complexity quickSortFirst  O(n^2) -- worst case
+         * Time complexity quickSortMedian  O(n*log(n)) -- average case
+         */
+        runTest("An array with values in reversed order", reversedArrayOriginal, sorter);
+
+        System.out.println("\n--------------------------- END OF SORTING ---------------------------");
+
+    }
+
+    /**
+     * Runs both quick sort methods first and median pivot on the input array
+     *
+     * @param testName Test type description of array being passed
+     * @param originalArray The array to be sorted.
+     * @param arraySorter The QuickSort object.
+     */
+    public static void runTest(String testName, int[] originalArray, QuickSort arraySorter) {
+        System.out.println("\n--------------------------------------------------------");
+        System.out.printf(" TEST CASE: %s \n", testName);
+        System.out.println("--------------------------------------------------------");
+        System.out.printf("Original Array: %s\n", Arrays.toString(originalArray));
+        // Run Test first value as pivot
+        int[] arrayForFirstPivot = Arrays.copyOf(originalArray, originalArray.length);
+        long startTimeFirst = System.nanoTime();
+        arraySorter.quickSortFirst(arrayForFirstPivot);
+        long endTimeFirst = System.nanoTime();
+        long durationFirst = endTimeFirst - startTimeFirst;
+        // Run Test of median of 3 random values as pivot 
+        int[] arrayForMedianPivot = Arrays.copyOf(originalArray, originalArray.length);
+        long startTimeMedian = System.nanoTime();
+        arraySorter.quickSortMedian(arrayForMedianPivot);
+        long endTimeMedian = System.nanoTime();
+        long durationMedian = endTimeMedian - startTimeMedian;
+        // Print the table
+        System.out.printf("Sorted Array:   %s\n\n", Arrays.toString(arrayForFirstPivot));
+        System.out.print("|----------------------------------------|\n");
+        System.out.printf("| %-20s | %15s |\n", "Pivot Strategy", "Time (ns)");
+        System.out.print("|----------------------------------------|\n");
+        System.out.printf("| %-20s | %,15d |\n", "First Element", durationFirst);
+        System.out.printf("| %-20s | %,15d |\n", "Median of Three", durationMedian);
+        System.out.print("|----------------------------------------|\n");
+    }
+}
---
+++ src/QuickSort.java
@@ -0,0 +1,190 @@
+import java.util.Random;
+
+/**
+ * Implements two different QuickSort algorithms: one using the first element as the pivot,
+ * and the other using the median of three random elements as the pivot.
+ * Both methods break the array into two sub-arrays and recursively sort them.
+ * 
+ */
+public class QuickSort {
+
+    /**
+     * Sorts the provided array  using QuickSort using first element as the pivot.
+     *
+     * @param inputArray the array to sort; if null or empty, the method returns immediately
+     */
+    public void quickSortFirst(int[] inputArray) {
+        // check if array is null or empty
+        if (inputArray == null || inputArray.length == 0) {
+            return;
+        } else {
+            // Call the recursive quicksort method staring low at 0 and high at length - 1
+            quicksortFirstPivot(inputArray, 0, inputArray.length - 1);
+        }
+    }
+
+    /**
+     * Recursive QuickSort method implementation which partitions using a first element pivot
+     * First, it checks if the lowIndex is less than the highIndex check for base case
+     * If it is not a base case, it calls the partition method to partition the array
+     * The pivotIndex returned from the partition method is then used to separate the array into two sub-arrays
+     * The method then recursively calls itself on the left and right sub-arrays until the base case is reached
+     *
+     * @param inputArray the array being sorted
+     * @param lowIndex   the starting index of the subarray
+     * @param highIndex  the ending index of the subarray
+     */
+    private void quicksortFirstPivot(int[] inputArray, int lowIndex, int highIndex) {
+        // Initial check to see if lowIndex is less than highIndex
+        if (lowIndex < highIndex) {
+            // partitionIndex divides the array into two sub-arrays
+            int partitionIndex  = partitionArray(inputArray, lowIndex, highIndex);
+            // Recursively sort
+            // sort the left sub-array
+            quicksortFirstPivot(inputArray, lowIndex, partitionIndex  - 1);
+            // sort the right sub-array
+            quicksortFirstPivot(inputArray, partitionIndex  + 1, highIndex);
+        }
+    }
+
+    /**
+     * Method loops through the array and partitions it around a pivot value
+     * The pivot is chosen as the first element in the subarray at lowIndex.
+     * Compares each element in the subarray to the pivot value and swaps elements 
+     * when pivot value is greater than current element value
+     * Swaps the pivot value with the element at the partition index
+     *
+     * @param inputArray the array containing the subarray to partition
+     * @param lowIndex   the starting index of the subarray (pivot location)
+     * @param highIndex  the ending index of the subarray
+     * @return the final index of the pivot after partitioning
+     */
+    private int partitionArray(int[] inputArray, int lowIndex, int highIndex) {
+        // assign the pivot value to the first value in array at lowIndex
+        int pivotValue = inputArray[lowIndex];
+        // i starts from the low index
+        int partitionIndex  = lowIndex;
+        // interates though the array from lowIndex + 1 to highIndex
+        for (int currentIndex  = lowIndex + 1; currentIndex  <= highIndex; currentIndex ++) {
+            // Checks if current element value is less or equal to pivot value
+            if (inputArray[currentIndex] < pivotValue) {
+                //  increment partition Index to move to next position in array 
+                partitionIndex++;
+                // swap the elements
+                swap(inputArray, partitionIndex, currentIndex);
+                
+            }
+        }
+        // Swap the lowIndex with the index partitionIndex the pivot position
+        swap(inputArray, lowIndex, partitionIndex);
+        return partitionIndex;
+    }
+
+
+    /**
+     * Sorts array using QuickSort with a pivot chosen as the median of three
+     *
+     * @param inputArray the array that needs to be sorted
+     */
+    public void quickSortMedian(int[] inputArray) {
+        // check if array is null or empty
+        if (inputArray == null || inputArray.length == 0) {
+            return;
+        } else {
+            // Call the recursive quicksort method staring low at 0 and high at length - 1
+            quicksortMedianPivot(inputArray, 0, inputArray.length - 1);
+        }
+    }
+
+
+    /**
+     * Recursive QuickSort implementation which partitions using a median-of-three-random pivot.
+     * First, it checks if the lowIndex is less than the highIndex check for base case
+     * If it is not a base case, it calls the partition method to partition the array
+     * The pivotIndex returned from the partition method is then used to separate the array into two sub-arrays
+     * The method then recursively calls itself on the left and right sub-arrays until the base case is reached
+     * 
+     * @param inputArray the array being sorted
+     * @param low  starting index of the subarray
+     * @param high  ending index of the subarray
+     */
+    private void quicksortMedianPivot(int[] inputArray, int lowIndex, int highIndex) {
+        if (lowIndex < highIndex) {
+            // Find median pivot and partition the array
+            int partitionIndex = partitionMedianPivot(inputArray, lowIndex, highIndex);
+            // Recursively sort
+            // sort the left sub-array
+            quicksortMedianPivot(inputArray, lowIndex, partitionIndex - 1);
+            // sort the right sub-array
+            quicksortMedianPivot(inputArray, partitionIndex + 1, highIndex);
+        }
+    }
+
+    /**
+     * Partitions the subarray using a median-of-three as a pivot.
+     * First uses the findAndPlaceMedianPivot method to find the median pivot
+     * Then using the same partition logic to break the array into two sub-arrays
+     *
+     * @param inputArray the array containing the subarray to partition
+     * @param low starting index of the subarray
+     * @param high ending index of the subarray
+     * @return index of the pivot after partitioning
+     */
+    private int partitionMedianPivot(int[] inputArray, int lowIndex, int highIndex) {
+        // Find the median of three random indexes and place it at the lowIndex
+        findAndPlaceMedianPivot(inputArray, lowIndex, highIndex);
+
+        // Now that the pivot is at arr[low], we can use the same partition logic
+        int partitionIndex = partitionArray(inputArray, lowIndex, highIndex);
+        return partitionIndex;
+    }
+
+    /**
+     * Finds the median of three random elements in the subarray
+     * Swaps that median value into position first position so it becomes the pivot.
+     *
+     * @param inputArray the array containing the values
+     * @param low        the starting index of the subarray
+     * @param high       the ending index of the subarray
+     */
+    private void findAndPlaceMedianPivot(int[] inputArray, int lowIndex, int highIndex) {
+        // Check if at least 3 elements in array
+        if (highIndex - lowIndex < 2) {
+            return;
+        }
+        Random rand = new Random();
+        // Choose three random index locations in input array
+        int indexOne = lowIndex + rand.nextInt(highIndex - lowIndex + 1);
+        int indexTwo = lowIndex + rand.nextInt(highIndex - lowIndex + 1);
+        int indexThree = lowIndex + rand.nextInt(highIndex - lowIndex + 1);
+        // Get the values for three random indexes
+        int valOne = inputArray[indexOne];
+        int valTwo = inputArray[indexTwo];
+        int valThree = inputArray[indexThree];
+
+        int medianIndex;
+        // Finding the median value
+        if ((valOne >= valTwo && valOne <= valThree) || (valOne <= valTwo && valOne >= valThree)) {
+            medianIndex = indexOne;
+        } else if ((valTwo >= valOne && valTwo <= valThree) || (valTwo <= valOne && valTwo >= valThree)) {
+            medianIndex = indexTwo;
+        } else {
+            medianIndex = indexThree;
+        }
+        // Swap the median element with the first element to be used as the pivot in partitioning method
+        swap(inputArray, lowIndex, medianIndex);
+    }
+
+    /**
+     * Swaps two elements locations in an array.
+     *
+     * @param inputArray the array containing the elements
+     * @param i index of the first element
+     * @param j index of the second element
+     */
+    private void swap(int[] inputArray, int positionOne, int positionTwo) {
+        int temp = inputArray[positionOne];
+        inputArray[positionOne] = inputArray[positionTwo];
+        inputArray[positionTwo] = temp;
+    }
+}
